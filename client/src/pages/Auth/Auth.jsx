99d630839f4e44dfdaec68f6b6148960c36fe4f7
Gopi2301 <gopinaths2301@gmail.com>
2023-04-29 23:13:06 +0530

    login page outline

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import "./Auth.css";
+import icon from "../../assets/logo.png";
 
 const Auth = () => {
   const [isSignUp, setisSignUp] = useState(false);
   return (
     <section className="auth-section">
       <div className="auth-container">
-        {/* {!isSignUp && <img src={icon} alt='stack overflow' className="login-logo"/>} */}
+        {!isSignUp && (
+          <img src={icon} alt="stack overflow" className="login-logo" />
+        )}
+        <form>
+          <label htmlFor="">
+            <h4>Email</h4>
+            <input type="email" name="name" id="email" />
+          </label>
+          <label htmlFor="password">
+            <div>
+              <h4>Password</h4>
+              <h4>forget password</h4>
+            </div>
+            <input type="password" name="name" id="password" />
+          </label>
+          <button type="submit" className="auth-btn">
+            {isSignUp ? "SignUp" : "LogIn"}
+          </button>
+        </form>
       </div>
     </section>
   );
